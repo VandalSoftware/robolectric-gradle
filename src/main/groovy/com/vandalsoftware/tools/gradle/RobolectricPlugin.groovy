0240367542d3085570b1d23836a203fe03237ff9
Jonathan Le <jle@vandal.io>
2014-11-05 12:54:04 -0800

    Extract constants

--- src/main/groovy/com/vandalsoftware/tools/gradle/RobolectricPlugin.groovy
+++ src/main/groovy/com/vandalsoftware/tools/gradle/RobolectricPlugin.groovy
@@ -11,6 +11,18 @@ import org.gradle.api.tasks.compile.JavaCompile
 import org.gradle.api.tasks.testing.Test
 
 class RobolectricPlugin implements Plugin<Project> {
+    public static final String COMPILE_TASK_NAME = 'compile'
+    public static final String TEST_TASK_NAME = 'test'
+    public static final String TASK_NAME_PREFIX = 'robolectric'
+
+    public static final String TEST_COMPILE_CONFIGURATION_NAME = 'testCompile'
+
+    public static final String JUNIT_JAR_VERSION = '4.11'
+    public static final String ROBOLECTRIC_JAR_VERSION = "2.3"
+
+    public static final String VERIFICATION_GROUP_NAME = 'Verification'
+    public static final String CHECK_TASK_NAME = 'check'
+
     @Override
     void apply(Project project) {
         def variants
@@ -26,8 +38,8 @@ class RobolectricPlugin implements Plugin<Project> {
                     " com.android.application or com.android.library")
         }
 
-        def compileConfig = project.configurations.getByName('compile')
-        Configuration testConfig = project.configurations.create('testCompile', {
+        def compileConfig = project.configurations.getByName(COMPILE_TASK_NAME)
+        Configuration testConfig = project.configurations.create(TEST_COMPILE_CONFIGURATION_NAME, {
             extendsFrom compileConfig
         })
 
@@ -37,12 +49,12 @@ class RobolectricPlugin implements Plugin<Project> {
                 deps.add("$dep.group:$dep.name".toString())
             }
             if (!deps.contains('junit:junit')) {
-                project.dependencies.add(testConfig.name, 'junit:junit:4.11', {
+                project.dependencies.add(testConfig.name, "junit:junit:$JUNIT_JAR_VERSION", {
                     exclude module: 'hamcrest-core'
                 })
             }
             if (!deps.contains('org.robolectric:robolectric')) {
-                project.dependencies.add(testConfig.name, 'org.robolectric:robolectric:2.3', {
+                project.dependencies.add(testConfig.name, "org.robolectric:robolectric:$ROBOLECTRIC_JAR_VERSION", {
                     exclude module: 'commons-logging'
                     exclude module: 'httpclient'
                 })
@@ -55,7 +67,7 @@ class RobolectricPlugin implements Plugin<Project> {
             })
         }
 
-        def testAll = project.tasks.create(name: 'test', group: 'Verification',
+        def testAll = project.tasks.create(name: TEST_TASK_NAME, group: VERIFICATION_GROUP_NAME,
                 description: 'Runs all Robolectric tests')
 
         variants.all { variant ->
@@ -89,7 +101,7 @@ class RobolectricPlugin implements Plugin<Project> {
             def testVariantOutputDirName = testVariant.toLowerCase()
             def testDestinationDir =
                     project.file("${project.buildDir}/test-classes/$testVariantOutputDirName")
-            def compileTask = project.tasks.create("robolectricCompile${testVariant}TestJava",
+            def compileTask = project.tasks.create("${TASK_NAME_PREFIX}Compile${testVariant}TestJava",
                     JavaCompile)
             compileTask.dependsOn variantJavaCompile
             compileTask.classpath = testClasspath
@@ -99,7 +111,7 @@ class RobolectricPlugin implements Plugin<Project> {
                 compileTask.options.bootClasspath = plugin.bootClasspath.join(File.pathSeparator)
             }
 
-            def procResTask = project.tasks.create("robolectricProcess${testVariant}TestResources",
+            def procResTask = project.tasks.create("${TASK_NAME_PREFIX}Process${testVariant}TestResources",
                     Copy)
             procResTask.from('src') {
                 include 'test/resources/**/*'
@@ -110,10 +122,10 @@ class RobolectricPlugin implements Plugin<Project> {
             procResTask.destinationDir =
                     project.file("${project.buildDir}/test-resources/$testVariantOutputDirName")
 
-            def classesTask = project.tasks.create("robolectric${testVariant}TestClasses")
+            def classesTask = project.tasks.create("${TASK_NAME_PREFIX}${testVariant}TestClasses")
             classesTask.dependsOn procResTask, compileTask
 
-            def testTask = project.tasks.create("robolectricTest${testVariant}", Test)
+            def testTask = project.tasks.create("${TASK_NAME_PREFIX}Test${testVariant}", Test)
             testTask.dependsOn classesTask
 
             def testResultsOutput =
@@ -144,12 +156,13 @@ class RobolectricPlugin implements Plugin<Project> {
             testTask.systemProperties.put('android.manifest', processedManifestPath)
             testTask.systemProperties.put('android.resources', processedResourcesPath)
             testTask.systemProperties.put('android.assets', processedAssetsPath)
+
             // Work around http://issues.gradle.org/browse/GRADLE-1682
             testTask.scanForTestClasses = false
 
             testAll.dependsOn testTask
         }
-        def checkTask = project.tasks.findByName('check')
+        def checkTask = project.tasks.findByName(CHECK_TASK_NAME)
         if (checkTask) {
             checkTask.dependsOn testAll
         }
